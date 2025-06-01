b6f8d2f08d80c36fbd85973380abccb26411a7ed
Ilyesadd <Elyasaddoun@gmail.com>
2025-06-01 23:18:34 +0200

    Modifié le fichier ticketmaster.sql

--- ticketmaster.sql
+++ ticketmaster.sql
@@ -35,7 +35,7 @@ CREATE TABLE `messages` (
   `message` text NOT NULL,
   `status` enum('unread','read') NOT NULL DEFAULT 'unread',
   `sent_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
@@ -48,7 +48,7 @@ CREATE TABLE `orders` (
   `user_id` int DEFAULT NULL,
   `ticket_id` int DEFAULT NULL,
   `purchase_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
@@ -64,7 +64,7 @@ CREATE TABLE `tickets` (
   `location` varchar(100) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `status` enum('National','International') NOT NULL DEFAULT 'National'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB;
 
 --
 -- Déchargement des données de la table `tickets`
@@ -97,7 +97,7 @@ CREATE TABLE `ticket_messages` (
   CONSTRAINT `ticket_messages_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`) ON DELETE CASCADE,
   CONSTRAINT `ticket_messages_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   CONSTRAINT `ticket_messages_ibfk_3` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB;
 
 -- --------------------------------------------------------
 
@@ -111,7 +111,7 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
   `is_admin` tinyint(1) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB;
 
 --
 -- Déchargement des données de la table `users`
